3b099bf144c417efa2f35f36520d668a3e849161
Aaron Honeycutt <aaron@system76.com>
2023-11-07 18:27:21 -0700

    cosmic-edit: unstable-2023-11-02 -> unstable-2023-11-29

--- pkgs/applications/editors/cosmic-edit/default.nix
+++ pkgs/applications/editors/cosmic-edit/default.nix
@@ -4,7 +4,7 @@
 , rust
 , rustPlatform
 , cmake
-, makeWrapper
+, makeBinaryWrapper
 , cosmic-icons
 , just
 , pkg-config
@@ -24,21 +24,22 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cosmic-edit";
-  version = "unstable-2023-11-02";
+  version = "unstable-2023-11-10";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-edit";
-    rev = "ee2dea71e6e21967bc705046f9650407d07cdada";
-    sha256 = "sha256-27j9Imlyzwy2yjpNsWWcX0qW38ZxMDkht1Eaggr4NYY=";
+    rev = "c66a28320a5274d62d68a6d90567bcf4d3e9666c";
+    hash = "sha256-QmIF2QAKx/dImKLy2qXJHIBs+/JhbHXE4FTqR2FClrA=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
       "accesskit-0.11.0" = "sha256-xVhe6adUb8VmwIKKjHxwCwOo5Y1p3Or3ylcJJdLDrrE=";
-      "cosmic-config-0.1.0" = "sha256-VKvJ7cNwMLLs6ElpgP6xwsBUnD5MDApwWl7rrb/Lr/U=";
-      "cosmic-text-0.10.0" = "sha256-g9y2qZ2ivpDtZAtsXqMXgGjUFi9QZx/weMhk7hmnB3E=";
+      "cosmic-config-0.1.0" = "sha256-IyxAnQCR3btqBeQf0ZRozGXCz+xFiiT/oyaUUX+rjQY=";
+      "cosmic-text-0.10.0" = "sha256-8YqRog4/QBijOvWzkMjKHm859WxkrOUOc9Ubze6Ypyo";
+      "modit-0.1.0" = "sha256-skTUarsaBLWl0LSxzJQ2MdFGvcKPAbazRdss4Q+PeT8=";
       "sctk-adwaita-0.5.4" = "sha256-yK0F2w/0nxyKrSiHZbx7+aPNY2vlFs7s8nu/COp2KqQ=";
       "smithay-client-toolkit-0.16.1" = "sha256-z7EZThbh7YmKzAACv181zaEZmWxTrMkFRzP0nfsHK6c=";
       "softbuffer-0.2.0" = "sha256-VD2GmxC58z7Qfu/L+sfENE+T8L40mvUKKSfgLmCTmjY=";
@@ -51,7 +52,7 @@ rustPlatform.buildRustPackage rec {
     substituteInPlace justfile --replace '#!/usr/bin/env' "#!$(command -v env)"
   '';
 
-  nativeBuildInputs = [ cmake just pkg-config which lld util-linuxMinimal makeWrapper ];
+  nativeBuildInputs = [ cmake just pkg-config which lld util-linuxMinimal makeBinaryWrapper ];
   buildInputs = [ libxkbcommon libinput fontconfig freetype wayland expat udev glib gtk3 ];
 
   dontUseJustBuild = true;
@@ -65,7 +66,7 @@ rustPlatform.buildRustPackage rec {
     "target/${rust.lib.toRustTargetSpecShort stdenv.hostPlatform}/release/cosmic-edit"
   ];
 
- postInstall = ''
+  postInstall = ''
     wrapProgram "$out/bin/${pname}" \
       --suffix XDG_DATA_DIRS : "${cosmic-icons}/share"
   '';
