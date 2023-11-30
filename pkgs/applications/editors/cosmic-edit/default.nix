08aa06071061ddb02e28720b0c7cd056a4d50e1b
a-kenji <aks.kenji@protonmail.com>
2023-11-30 22:03:47 +0100

    apply patch

--- pkgs/applications/editors/cosmic-edit/default.nix
+++ pkgs/applications/editors/cosmic-edit/default.nix
@@ -1,22 +1,23 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, rust
-, rustPlatform
-, cmake
-, makeBinaryWrapper
-, cosmic-icons
-, just
-, pkg-config
-, libxkbcommon
-, glib
-, gtk3
-, libinput
-, fontconfig
-, freetype
-, wayland
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  rust,
+  rustPlatform,
+  cmake,
+  makeBinaryWrapper,
+  cosmic-icons,
+  just,
+  pkg-config,
+  libxkbcommon,
+  glib,
+  gtk3,
+  libinput,
+  fontconfig,
+  freetype,
+  wayland,
+  xorg,
 }:
-
 rustPlatform.buildRustPackage rec {
   pname = "cosmic-edit";
   version = "unstable-2023-11-29";
@@ -24,19 +25,17 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-edit";
-    rev = "2dd2d8d4ed0802faa272a1f1ee0157d531a51e03";
-    hash = "sha256-J9bpSfTkVXa2mbpdSowwtzZPBvgMPpKRvxVpr0J4/CA=";
+    rev = "4a3dd101f35eb3c1c585f104d78ed4ee31d393d3";
+    hash = "sha256-pk+4u13oWZ4fgXy1tlDgq+E4J+UddjTNSexMm4dgBSo=";
   };
-
-  cargoHash = "";
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
       "accesskit-0.11.0" = "sha256-xVhe6adUb8VmwIKKjHxwCwOo5Y1p3Or3ylcJJdLDrrE=";
       "atomicwrites-0.4.2" = "sha256-QZSuGPrJXh+svMeFWqAXoqZQxLq/WfIiamqvjJNVhxA=";
-      "cosmic-config-0.1.0" = "sha256-a136DDSEiUxBZ0l4I2L3SYvGv+a5dz7S3azNsWdBdnE=";
-      "cosmic-text-0.10.0" = "sha256-Je2k2U5u4QH8F/QKE6ZnDfAfdN2R//t+4l3mS5RIzt4=";
+      "cosmic-config-0.1.0" = "sha256-wBliqZbRHYiwZmu0vHeIP5DFzg/1IeQP3aMxiYC88bo=";
+      "cosmic-text-0.10.0" = "sha256-fE5HkhITLw0OBfFLFMsKEJw5idO265i4S7qylHTt7C0=";
       "sctk-adwaita-0.5.4" = "sha256-yK0F2w/0nxyKrSiHZbx7+aPNY2vlFs7s8nu/COp2KqQ=";
       "softbuffer-0.3.3" = "sha256-eKYFVr6C1+X6ulidHIu9SP591rJxStxwL9uMiqnXx4k=";
       "smithay-client-toolkit-0.16.1" = "sha256-z7EZThbh7YmKzAACv181zaEZmWxTrMkFRzP0nfsHK6c=";
@@ -50,8 +49,22 @@ rustPlatform.buildRustPackage rec {
     substituteInPlace justfile --replace '#!/usr/bin/env' "#!$(command -v env)"
   '';
 
-  nativeBuildInputs = [ cmake just pkg-config makeBinaryWrapper ];
-  buildInputs = [ libxkbcommon libinput fontconfig freetype wayland glib gtk3 ];
+  nativeBuildInputs = [
+    cmake
+    just
+    pkg-config
+    makeBinaryWrapper
+  ];
+  buildInputs = [
+    libxkbcommon
+    xorg.libX11
+    libinput
+    fontconfig
+    freetype
+    wayland
+    glib
+    gtk3
+  ];
 
   dontUseJustBuild = true;
 
@@ -61,7 +74,9 @@ rustPlatform.buildRustPackage rec {
     (placeholder "out")
     "--set"
     "bin-src"
-    "target/${rust.lib.toRustTargetSpecShort stdenv.hostPlatform}/release/cosmic-edit"
+    "target/${
+      rust.lib.toRustTargetSpecShort stdenv.hostPlatform
+    }/release/cosmic-edit"
   ];
 
   postInstall = ''
